63cf0e752c3801756af1b56e4199f6c6aa50b5dc
astherath <farceriv@gmail.com>
2019-02-28 13:55:21 -0500

    added mild documentation for the stable, working prototype

--- main.go
+++ main.go
@@ -8,15 +8,21 @@ import (
 )
 
 func main() {
+	// stores pathname of the json file to pass into the packages
 	pathname := "test.json"
 
+	// create a matches struct (derived from parser pkg)
 	var matches parser.Matches
 
+	// parses the json file with the given pathname and stores the result
 	matches = parser.Parse(pathname)
 
+	// creates a string var to hold the result of the handler
 	var response string
 
+	// passes the matches into the handler and stores the string returned
 	response = handler.HandleMatches(matches)
 
+	// prints the string with all the match info in it
 	fmt.Println(response)
 }
