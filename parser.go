8ae815c6c938d7d4ced5605e628cdb773072ca03
astherath <farceriv@gmail.com>
2019-02-28 12:24:11 -0500

    first commit of new package made to parse the JSON input file

---
+++ parser.go
@@ -0,0 +1,43 @@
+package parser
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"encoding/json"
+)
+
+// create struct to get valuable data from json file
+type Match struct {
+	Name     string
+	Begin_at string
+	Status   string
+}
+
+type Matches struct {
+	Matches []Match
+}
+
+func parse() {
+	pathname := "test.json"
+
+	// read the json file in the pathname given as a byte array
+	fileArray, er := ioutil.ReadFile(pathname)
+
+	// error handling for the file reading
+	if er != nil {
+	    fmt.Println("error reading the file using ioutil: ", er)
+	}
+
+	// also initializes a matches struct to store our data
+	var matches Matches
+
+	// unmarshal json file into the struct we've created
+	eror := json.Unmarshal(fileArray, &matches)
+
+	// error handling again for the json file marshaling
+	if eror != nil {
+	    fmt.Println("error here again pally: ", eror)
+	}
+
+}
