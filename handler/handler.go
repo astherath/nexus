248e1b86a364ffe01bbca68a2014d489fcd6276d
astherath <farceriv@gmail.com>
2019-03-01 20:04:35 -0500

    minor change to how date function is implemented. Will start to lay gorundwork for new date function implementation soon

--- handler/handler.go
+++ handler/handler.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/astherath/lcs_app/parser"
 )
 
@@ -16,8 +19,37 @@ func HandleMatches(ms parser.Matches) string {
 		// concats the string
 		matchInfo += "Match name: " + current.Name + "\n"
 		matchInfo += "Match status: " + current.Status + "\n"
-		matchInfo += "Match Starts at: " + current.Begin_at + "\n\n"
+
+		// stores year
+		year := readDate(current)
+
+		matchInfo += "Match Starts at: " + year + "\n\n"
 	}
 
 	return matchInfo
+}
+
+func readDate(m parser.Match) string {
+
+	// converts  the "begin_at" part of the match
+	dateString := m.Begin_at
+
+	// store our current format
+	parseWith := "2006-01-02T15:04:05Z"
+
+	// parse the time string and error check
+	t, err := time.Parse(parseWith, dateString)
+	if err != nil {
+		fmt.Println("error parsing time: ", err)
+	}
+
+	// store our wanted format
+	form := "Mon, January 2, 3 PM"
+
+	// format out time into our new format
+	post := t.Format(form)
+
+	// return the week number (relative to the first week given)
+
+	return string(post)
 }
