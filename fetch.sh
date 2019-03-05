018d18fb337c937e7c4ba768b9cfd97611e7ad04
astherath <farceriv@gmail.com>
2019-03-04 23:12:01 -0500

    created bash script to fetch the data from the API as well as format it to be used

---
+++ fetch.sh
@@ -0,0 +1,12 @@
+# writes the top portion of the json file
+echo "{\"matches\":" > test.json
+
+# uses cURL to fetch the json file from the api and writes it to the json file
+curl -sg -H 'Authorization: Bearer 8VnQ3mOjbj6arh_XBR4Pwv1cHdUZsyRr-552YTOl7ECffjPxRss' 'https://api.pandascore.co/leagues/league-of-legends-lec/matches?filter[finished]=false&sort=begin_at' >> test.json
+
+# appends the closing tag to the json file
+echo "}" >> test.json
+
+# returns this command if everythign executed properly
+echo "Fetch Complete"
+
