f4fa2efa022a43f771e2277d34e8f459d4a49d3c
astherath <farceriv@gmail.com>
2019-03-10 00:43:20 -0500

    created barebones version of the cobra instance, minus the import from our temporary main.go file

--- commands/root.go
+++ commands/root.go
@@ -1,7 +1,27 @@
 package commands
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/spf13/cobra"
+)
 
-func main() {
-	fmt.Println("vim-go")
-}
+// initializing the root command with it's initial options and flags
+var RootCommand = &cobra.Command{
+
+    // name of the app
+    Use: "Nexus esports tool",
+
+    // short desc of the app
+    Short : "Displays LoL pro match data",
+
+    // long desc
+    Long: 'Tool to fetch and display data of upcoming professional
+    	League of Legends matches from the major pro regions',
+
+    // run main func here
+    Run: func(cmd *cobra.Command, args []string) {
+	// TODO import from temp main func
+
+
+
+
