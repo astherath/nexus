c4c1c5badaf224e5365d2c67b17a6b6c1fc017c4
astherath <farceriv@gmail.com>
2019-03-10 00:50:03 -0500

    shifted runnable code from main to the commands app

--- commands/root.go
+++ commands/root.go
@@ -1,27 +1,45 @@
 package commands
 
 import (
-    "fmt"
-    "github.com/spf13/cobra"
+	"fmt"
+
+	"github.com/astherath/lcs_app/handler"
+	"github.com/astherath/lcs_app/parser"
+	"github.com/spf13/cobra"
 )
 
 // initializing the root command with it's initial options and flags
 var RootCommand = &cobra.Command{
 
-    // name of the app
-    Use: "Nexus esports tool",
+	// name of the app
+	Use: "Nexus esports tool",
 
-    // short desc of the app
-    Short : "Displays LoL pro match data",
+	// short desc of the app
+	Short: "Displays LoL pro match data",
 
-    // long desc
-    Long: 'Tool to fetch and display data of upcoming professional
-    	League of Legends matches from the major pro regions',
+	// long desc
+	Long: `Tool to fetch and display data of upcoming professional
+    	League of Legends matches from the major pro regions`,
 
-    // run main func here
-    Run: func(cmd *cobra.Command, args []string) {
-	// TODO import from temp main func
+	// run main func here
+	Run: func(cmd *cobra.Command, args []string) {
 
+		// stores pathname of the json file to pass into the packages
+		pathname := "test.json"
 
+		// create a matches struct (derived from parser pkg)
+		var matches parser.Matches
 
+		// parses the json file with the given pathname and stores the result
+		matches = parser.Parse(pathname)
 
+		// creates a string var to hold the result of the handler
+		var response string
+
+		// passes the matches into the handler and stores the string returned
+		response = handler.HandleMatches(matches)
+
+		// prints the string with all the match info in it
+		fmt.Println(response)
+	},
+}
