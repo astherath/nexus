674f0c79c30bae9c21ec65aedbf115cf638e0c65
astherath <farceriv@gmail.com>
2019-03-13 11:07:04 -0400

    added a new flag to show all matches

--- commands/upcoming.go
+++ commands/upcoming.go
@@ -17,16 +17,31 @@ package commands
 import (
 	"fmt"
 
+	"github.com/astherath/nexus/handler"
+	"github.com/astherath/nexus/parser"
 	"github.com/spf13/cobra"
 )
 
 // upcomingCmd represents the upcoming command
 var upcomingCmd = &cobra.Command{
-	Use:   "upcoming -a",
+	Use:   "upcoming",
 	Short: "Displays the upcoming LEC matches ",
 	Long:  `Shows the upcoming matches for the LEC 2019 Spring Split`,
+	// use RunE to throw an error if the user calls this command without any arguments
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("upcoming called")
+		all, err := cmd.Flags().GetBool("all")
+
+		// check if user wants all matches displayed
+		if all {
+			// call func to show all
+			fmt.Println("used all flag")
+			showAll()
+		}
+
+		if err != nil {
+			fmt.Println("error in all flag: ", err)
+		}
+
 	},
 }
 
@@ -44,5 +59,26 @@ func init() {
 	// upcomingCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 	// set a flag to display all weeks in the split
-	upcomingCmd.Flags().BoolP("toggle", "a", false, "Show all upcoming weeks")
+	upcomingCmd.Flags().BoolP("all", "a", false, "Show all upcoming weeks")
+
+}
+
+func showAll() {
+	// stores pathname of the json file to pass into the packages
+	pathname := "/Users/felipearce/go/src/github.com/astherath/nexus/test.json"
+
+	// create a matches struct (derived from parser pkg)
+	var matches parser.Matches
+
+	// parses the json file with the given pathname and stores the result
+	matches = parser.Parse(pathname)
+
+	// creates a string var to hold the result of the handler
+	var response string
+
+	// passes the matches into the handler and stores the string returned
+	response = handler.HandleMatches(matches)
+
+	// prints the string with all the match info in it
+	fmt.Println(response)
 }
