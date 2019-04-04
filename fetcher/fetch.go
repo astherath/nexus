085bb3c1e33ef7f529ebc9ba12150715a45328f5
astherath <farceriv@gmail.com>
2019-04-04 15:01:30 -0400

    created a map to enable the selection of different teams

--- fetcher/fetch.go
+++ fetcher/fetch.go
@@ -15,23 +15,42 @@
 package fetcher
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
+// constants
 var LCK int = 1602
 var LEC int = 1704
 var LCS int = 1705
+var leagues map[string]int
 
 // cURL func for the api to get exported
-func CURL() {
+func CURL(league string) error {
+	// create the map to be used for the league selection
+	leagues = make(map[string]int)
+	leagues["LCK"] = LCK
+	leagues["LEC"] = LEC
+	leagues["LCS"] = LCS
+
+	league = strings.ToUpper(league)
+
+	// get the league id from the league string
+	id := leagues[league]
+
+	if id == 0 {
+		return errors.New("Region name not found. Please enter the abreviation of a major league (e.g. LCK, LEC, LCS)")
+	}
+
 	// saving our token instead of using a header
 	token := "8VnQ3mOjbj6arh_XBR4Pwv1cHdUZsyRr-552YTOl7ECffjPxRss"
-	// url to access TODO: let this take different params
-	url := fmt.Sprintf("https://api.pandascore.co/series/%d/matches?filter[number_of_games]=5&sort=begin_at", LEC)
+	// url to access
+	url := fmt.Sprintf("https://api.pandascore.co/series/%d/matches?filter[number_of_games]=5&sort=begin_at", id)
 
 	// opens a http client in order to set a header and sets the timeout to 20 seconds
 	client := &http.Client{Timeout: time.Second * 20}
@@ -40,6 +59,7 @@ func CURL() {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		fmt.Println("error using http new request: ", err)
+		return err
 	}
 
 	// add headers to request
@@ -49,12 +69,14 @@ func CURL() {
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("error using client do with req: ", err)
+		return err
 	}
 
 	// create json file to write response to
 	file, err := os.Create("matches.json")
 	if err != nil {
 		fmt.Println("error when creating file ", err)
+		return err
 	}
 	// read the response of the http call
 	body, _ := ioutil.ReadAll(resp.Body)
@@ -68,14 +90,16 @@ func CURL() {
 	resp.Body.Close()
 	file.Close()
 
-	getTeams()
+	getTeams(id)
+
+	return nil
 }
 
-func getTeams() {
+func getTeams(id int) {
 	// saving our token instead of using a header
 	token := "8VnQ3mOjbj6arh_XBR4Pwv1cHdUZsyRr-552YTOl7ECffjPxRss"
-	// url to access TODO: let this take different params
-	url := fmt.Sprintf("https://api.pandascore.co/lol/series/%d/teams", LEC)
+	// url to access
+	url := fmt.Sprintf("https://api.pandascore.co/lol/series/%d/teams", id)
 
 	// opens a http client in order to set a header and sets the timeout to 20 seconds
 	client := &http.Client{Timeout: time.Second * 20}
